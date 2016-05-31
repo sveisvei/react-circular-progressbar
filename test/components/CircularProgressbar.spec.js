cb677f20180598bb5a07132ab1b2788ba5aea5ff
Kevin Qi <iqnivek@gmail.com>
2016-05-30 22:49:36 -0400

    Remove stuff from gh-pages branch

--- test/components/CircularProgressbar.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { assert } from 'chai';
-import CircularProgressbar from '../../src';
-
-describe('CircularProgressbar', () => {
-  it('should not throw exceptions in base case', () => {
-    assert.doesNotThrow(() => <CircularProgressbar />);
-  });
-});
